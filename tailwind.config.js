18ec7497670414027181f3eabecb16db5d5e58fb
Tahsin <tahsinfaruque@Tahsins-MacBook-Air.local>
2025-01-31 19:55:58 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+};
